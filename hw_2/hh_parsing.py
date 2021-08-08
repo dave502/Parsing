9784c1486e8fb40bb0f023e0ecd2957e5d383a39
Dave <davtyan.d.n@mail.ru>
2021-08-08 10:27:25 +0200

    Create hh_parsing.py

---
+++ hw_2/hh_parsing.py
@@ -0,0 +1,115 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+import pandas as pd
+
+class HHScraper:
+    __headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+    __url = 'https://hh.ru/'
+    __params = {}
+
+    def __init__(self):
+        pass
+
+    def __input_vacancy_text(self):
+        return input('Please enter vacancy key word: ')
+
+    def __get_employer(self, employer_text: str):
+        """returns valid employer name"""
+        if employer_text:
+            return employer_text.replace(u'\xa0', ' ')
+        else:
+            return None
+
+    def __get_location(self, location_text: str):
+        """returns valid location name"""
+        if location_text:
+            return location_text.split()[0]
+        else:
+            return None
+
+    def __get_salary_min_max(self, salary_text: str) -> list:
+        """returns the list with min and max salaries"""
+        salary_min = salary_max = None
+        if salary_text:
+            salary_text = re.sub(r'\s', '', salary_text)
+            start_max = salary_text.find('до')
+            start_min = salary_text.find('от')
+            # если сначала 'до' - salary_max присваиваем всё число из строки
+            if start_max > start_min:
+                salary_max = salary_text[start_max + len('до'):re.search(r'\d+', salary_text).end()]
+            else:  # в остальных слуаях ищем все числа, первое - минимальная зарплата, второе - максимальная
+                digits = re.findall(r'\d+', salary_text)
+                salary_min = digits[0] if len(digits) > 0 else None
+                salary_max = digits[1] if len(digits) == 2 else None
+        return [salary_min, salary_max]
+
+    def __get_salary_currency(self, salary_text: str) -> str:
+        salary_currency = None
+        if salary_text:
+            # получаем последнее "слово" в строке
+            salary_currency_text = re.split('\s+', salary_text)[-1]
+            # если "слово" начинается не с цифр - верояно это - валюта
+            if re.match(r'\D+', salary_currency_text).end():
+                salary_currency = salary_currency_text
+        return salary_currency
+
+
+    def __scrape_one_page(self, url: str) -> dict:
+        """returns dictionary {
+            'jobs': dictionary with jobs from the webpage,
+            'next_url': int with next page url link
+            }
+        """
+        response = requests.get(url, params=self.__params, headers=self.__headers)
+        soup = bs(response.text, 'html.parser')
+
+        job_div_list = soup.findAll('div', {'class': "vacancy-serp-item"})
+
+        job_data_list = []
+        for job_div in job_div_list:
+            job_data = {}
+            job_title_link = job_div.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})
+            job_data['title'] = job_title_link.getText() if job_title_link else None
+            job_data['link'] = job_title_link.get('href') if job_title_link else None
+            job_data['employer'] = self.__get_employer(
+                job_div.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'}).getText())
+            job_data['location'] = self.__get_location(
+                job_div.find('span', {'data-qa': "vacancy-serp__vacancy-address"}).getText())
+            span_salary = job_div.find('span', {'data-qa': "vacancy-serp__vacancy-compensation"})
+            job_data['salary_min'], job_data['salary_max'] = self.__get_salary_min_max(span_salary.getText()) \
+                if span_salary else [None, None]
+            job_data['salary_currency'] = self.__get_salary_currency(span_salary.getText()) if span_salary else None
+            job_data['source'] = self.__url
+            job_data_list.append(job_data)
+
+        # get Next button and next page number from its link
+        next_page = None
+        button_next = soup.findAll('a', {'data-qa': "pager-next"})
+        if button_next:
+            parsed = urlparse.urlparse(button_next[0].get('href'))
+            next_page = parse_qs(parsed.query)['page'][0]
+        return {'jobs': job_data_list, 'next_page': next_page}
+
+    def get_jobs_list(self):
+        """Gets jobs with inputted keyword from the site"""
+        job_data_list = []
+        self.__params = {'text': self.__input_vacancy_text()}
+        scraped_page_url = self.__url + 'search/vacancy'
+        while True:
+            scraped_page_data = self.__scrape_one_page(scraped_page_url)
+            job_data_list.extend(scraped_page_data['jobs'])
+            if scraped_page_data['next_page']:
+                self.__params['page'] = scraped_page_data['next_page']
+            else:
+                break
+
+        job_df = pd.DataFrame(job_data_list)
+        job_df.to_json("job_list.json")
+        print(job_df)
+
+scraper = HHScraper()
+scraper.get_jobs_list()
